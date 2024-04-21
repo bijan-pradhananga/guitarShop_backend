4eb8b6b4a221bb959ae7f22c1aa35d5cf8e1c186
Bijan <bijan2059@gmail.com>
2024-04-21 20:36:34 +0545

    added the rating router to main router

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,12 +3,14 @@ const categoryRouter = require('./categoryRouter')
 const productRouter = require('./productRoute')
 const userRouter = require('./userRoute')
 const cartRouter = require('./cartRouter')
+const ratingRouter = require('./ratingRouter')
 const router = express.Router()
 
 router.use('/user',userRouter)
 router.use('/category',categoryRouter)
 router.use('/product',productRouter)
 router.use('/cart',cartRouter)
+router.use('/rating',ratingRouter)
 
 
 module.exports = router
