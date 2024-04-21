63914de874321b7567dc398ad203ca9eb23b363f
Bijan <bijan2059@gmail.com>
2024-04-21 20:35:47 +0545

    added uploadMiddleware

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const Product = require('../controllers/productController')
+const UploadMiddleware = require('../middleware/UploadMiddleware')
 const pInstance = new Product()
+const fInstance = new UploadMiddleware();
+const upload = fInstance.upload('products');
 const productRouter = express.Router()
 
 productRouter.get('/', pInstance.index);
-productRouter.post('/', pInstance.store);
+productRouter.post('/', upload.single('image'), pInstance.store);
 productRouter.get('/:id', pInstance.show);
 productRouter.put('/:id', pInstance.update);
 productRouter.delete('/:id', pInstance.destroy);
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const User = require('../controllers/userController')
+const UploadMiddleware = require('../middleware/UploadMiddleware')
 const uInstance = new User()
 const userRouter = express.Router()
+const fInstance = new UploadMiddleware();
+const upload = fInstance.upload('users');
 
 userRouter.get('/', uInstance.index);
-userRouter.post('/', uInstance.store);
+userRouter.post('/',upload.single('image'), uInstance.store);
 userRouter.get('/:id', uInstance.show);
 userRouter.put('/:id', uInstance.update);
 userRouter.delete('/:id', uInstance.destroy);
