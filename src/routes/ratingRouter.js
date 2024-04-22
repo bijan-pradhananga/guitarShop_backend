4a494c8e6f65eb403ccc6b63a5c4ca68535bc690
Bijan <bijan2059@gmail.com>
2024-04-22 12:37:59 +0545

    removed rating router model and controller

--- src/routes/ratingRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const Rating = require('../controllers/ratingController');
-const ratingRouter = express.Router();
-const rInstance = new Rating();
-
-ratingRouter.post('/', rInstance.addRating);
-ratingRouter.get('/:product_id', rInstance.getRatingsByProduct);
-
-module.exports =  ratingRouter;
