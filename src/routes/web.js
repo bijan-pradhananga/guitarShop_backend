7587de05ce412d191064688b53c21699b5eef6a4
Bijan <bijan2059@gmail.com>
2024-04-24 13:31:38 +0545

    added auth route

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,11 +3,13 @@ const categoryRouter = require('./categoryRouter')
 const productRouter = require('./productRoute')
 const userRouter = require('./userRoute')
 const cartRouter = require('./cartRouter')
+const authRouter = require('./authRoute')
 const router = express.Router()
 
 router.use('/user',userRouter)
 router.use('/category',categoryRouter)
 router.use('/product',productRouter)
 router.use('/cart',cartRouter)
+router.use('/login',authRouter)
 
 module.exports = router
