1ec361368949d2cb8ef774d867b9a5a59079025d
Bijan <bijan2059@gmail.com>
2024-05-23 15:25:21 +0545

    added cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./routes/web')
 const Connection = require('./database/Connection')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3001
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser())
+app.use(cors({
+  credentials:true,
+  origin:['http://localhost:3000']
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(router);
@@ -16,8 +22,7 @@ app.use(router);
 new Connection()
 
 app.get('/',(req,res)=>{
-    res.send('hello')
-    console.log('hello');
+    res.send('hello to guitarShop Backend')
 })
 
 app.listen(port, () => {
