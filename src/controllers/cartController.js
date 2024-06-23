f8ba2c1fa4a06939aae8d685155cd44253b369bc
Bijan <bijan2059@gmail.com>
2024-06-23 14:59:06 +0545

    added error handling for cartController

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -4,45 +4,47 @@ class CartController {
     async addToCart(req, res) {
         try {
             const { product_id, user_id } = req.body;
-            // Check if the cart item already exists for the user
             const existingCartItem = await Cart.findOne({ product_id, user_id });
 
             if (existingCartItem) {
-                // If the cart item already exists, increment the quantity
                 existingCartItem.quantity += 1;
                 await existingCartItem.save();
-                res.status(200).json({ message: 'Cart updated successfully' });
+                return res.status(200).json({ message: 'Cart updated successfully' });
             } else {
-                // If the cart item doesn't exist, create a new one
                 await Cart.create({ product_id, user_id });
-                res.status(201).json({ message: 'Product added to cart successfully' });
+                return res.status(201).json({ success:true, message: 'Product added to cart successfully' });
             }
         } catch (err) {
-            res.status(500).json({ message: err.message });
+            if (!res.headersSent) {
+                res.status(500).json({ message: err.message });
+            }
         }
     }
 
     async removeFromCart(req, res) {
         try {
             const { product_id, user_id } = req.body;
-            // Find and delete the cart item for the specified product and user
             await Cart.findOneAndDelete({ product_id, user_id });
-            res.status(200).json({ message: 'Product removed from cart successfully' });
+            return res.status(200).json({ success:true, message: 'Product removed from cart successfully' });
         } catch (err) {
-            res.status(500).json({ message: err.message });
+            if (!res.headersSent) {
+                res.status(500).json({ message: err.message });
+            }
         }
     }
 
     async getCartItems(req, res) {
         try {
             const { user_id } = req.params;
-            // Find all cart items for the specified user
             const cartItems = await Cart.find({ user_id }).populate('product_id');
-            res.status(200).json(cartItems);
+            return res.status(200).json(cartItems);
         } catch (err) {
-            res.status(500).json({ message: err.message });
+            if (!res.headersSent) {
+                res.status(500).json({ success:true, message: err.message });
+            }
         }
     }
 }
 
 module.exports = CartController;
+
