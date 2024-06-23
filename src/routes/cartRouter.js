d8786a7a89eac9ac767bec5800c012f87a037a3f
Bijan <bijan2059@gmail.com>
2024-06-23 14:59:32 +0545

    added middleware in cartRouter

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Cart = require('../controllers/cartController');
+const checkAuth = require('../middleware/AuthMiddleware');
 const cartRouter = express.Router();
-const cartInstance = new Cart()
+const cartInstance = new Cart();
 
-cartRouter.post('/', cartInstance.addToCart);
-cartRouter.get('/:user_id', cartInstance.getCartItems);
-cartRouter.delete('/', cartInstance.removeFromCart);
+cartRouter.post('/',checkAuth, cartInstance.addToCart);
+cartRouter.get('/:user_id',checkAuth, cartInstance.getCartItems);
+cartRouter.delete('/',checkAuth, cartInstance.removeFromCart);
 
 module.exports =  cartRouter;
