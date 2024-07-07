bee9c15e8f142eca35d3e578515f4b5f33db41f4
Bijan <bijan2059@gmail.com>
2024-07-07 12:25:01 +0545

    added wishlist route to main route

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,7 @@ const productRouter = require('./productRoute')
 const userRouter = require('./userRoute')
 const cartRouter = require('./cartRouter')
 const authRouter = require('./authRoute')
+const wishlistRouter = require('./wishlistRoute')
 const router = express.Router()
 
 router.use('/user',userRouter)
@@ -11,5 +12,6 @@ router.use('/category',categoryRouter)
 router.use('/product',productRouter)
 router.use('/cart',cartRouter)
 router.use('/auth',authRouter)
+router.use('/wishlist',wishlistRouter)
 
 module.exports = router
