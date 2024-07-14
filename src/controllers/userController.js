5eb34277e864bfc63b2d5cc6e57c351a04831a87
Bijan <bijan2059@gmail.com>
2024-07-14 14:38:28 +0545

    added success msg

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -61,25 +61,25 @@ class UserController {
             // Find the user by ID
             const user = await User.findById(userId);
             if (!user) {
-                return res.status(404).json({ message: 'User not found' });
+                return res.status(404).json({success:false, message: 'User not found' });
             }
             // Check if the old password is correct
             const match = await comparePass(oldPassword, user.password);
             if (!match) {
-                return res.status(400).json({ message: 'Old password is incorrect' });
+                return res.status(400).json({success:false, message: 'Old password is incorrect' });
             }
             // Check if the new password and confirm password match
             if (newPassword !== confirmPassword) {
-                return res.status(400).json({ message: 'New password and confirm password do not match' });
+                return res.status(400).json({success:false, message: 'New password and confirm password do not match' });
             }
             // Hash the new password
             const hashedPassword = await hashPassword(newPassword, 10);
             // Update the user's password
             user.password = hashedPassword;
             await user.save();
-            res.status(200).json({ message: 'Password changed successfully' });
+            res.status(200).json({success:true, message: 'Password changed successfully' });
         } catch (err) {
-            res.status(500).json({ message: err.message });
+            res.status(500).json({success:false, message: err.message });
         }
     }
 }
