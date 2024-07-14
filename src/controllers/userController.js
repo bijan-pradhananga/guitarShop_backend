ec34892a0e01665e08482645401ec6349306fa31
Bijan <bijan2059@gmail.com>
2024-07-14 14:14:20 +0545

    made api to change password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const { hashPassword } = require("../helper/authHelper");
+const { hashPassword, comparePass } = require("../helper/authHelper");
 const User = require("../models/User");
 
 class UserController {
@@ -48,6 +48,36 @@ class UserController {
         try {
             await User.findByIdAndDelete(req.params.id);
             res.status(200).json({ message: 'User deleted successfully' });
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    }
+
+    async changePassword (req, res) {
+        const { oldPassword, newPassword, confirmPassword } = req.body;
+        const userId = req.params.id;
+
+        try {
+            // Find the user by ID
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            // Check if the old password is correct
+            const match = await comparePass(oldPassword, user.password);
+            if (!match) {
+                return res.status(400).json({ message: 'Old password is incorrect' });
+            }
+            // Check if the new password and confirm password match
+            if (newPassword !== confirmPassword) {
+                return res.status(400).json({ message: 'New password and confirm password do not match' });
+            }
+            // Hash the new password
+            const hashedPassword = await hashPassword(newPassword, 10);
+            // Update the user's password
+            user.password = hashedPassword;
+            await user.save();
+            res.status(200).json({ message: 'Password changed successfully' });
         } catch (err) {
             res.status(500).json({ message: err.message });
         }
