7c3f2f69eaab0a542e60d590e4eb31ecd0aa5194
Bijan <bijan2059@gmail.com>
2024-07-14 14:14:37 +0545

    added change pass api to userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../controllers/userController')
 const UploadMiddleware = require('../middleware/UploadMiddleware')
+const checkAuth = require('../middleware/AuthMiddleware')
 const uInstance = new User()
 const userRouter = express.Router()
 const fInstance = new UploadMiddleware();
@@ -11,5 +12,6 @@ userRouter.post('/',upload.single('image'), uInstance.store);
 userRouter.get('/:id', uInstance.show);
 userRouter.put('/:id', uInstance.update);
 userRouter.delete('/:id', uInstance.destroy);
+userRouter.put('/password/:id',checkAuth,uInstance.changePassword);
 
 module.exports =  userRouter;
