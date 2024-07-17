c70535e8ed3376e112415f5f79400622bf117024
Bijan <bijan2059@gmail.com>
2024-07-17 16:54:36 +0545

    minor update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,7 +31,7 @@ class UserController {
     async show(req, res) {
         try {
             const user = await User.findById(req.params.id);
-            res.status(200).json(user);
+            res.status(200).json({user});
         } catch (err) {
             res.status(500).json({ message: err.message });
         }
