9a3101d72befb4b0d85f2b72016490e8ce01bbf4
Bijan <bijan2059@gmail.com>
2024-08-21 19:03:53 +0545

    made order model

---
+++ src/models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [
+        {
+            product_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    total: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Completed', 'Cancelled'],
+        default: 'Pending'
+    }
+},{timestamps:true});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
