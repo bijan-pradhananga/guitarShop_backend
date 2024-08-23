6f660f53223f01b26c81d7cefb3dd7c28e9f80a4
Bijan <bijan2059@gmail.com>
2024-08-23 15:09:09 +0545

    added the new functions to route

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -5,6 +5,8 @@ const oInstance = new Order();
 
 orderRouter.post('/', oInstance.buyNow);
 orderRouter.get('/', oInstance.index);
+orderRouter.put('/:id', oInstance.update);
+orderRouter.put('/:id/cancel', oInstance.cancelOrder);
 orderRouter.post('/checkout', oInstance.checkout);
 orderRouter.get('/:id', oInstance.getUserOrders);
 
