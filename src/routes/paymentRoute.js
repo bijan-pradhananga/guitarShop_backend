319948843dc60ce983110f7b56016cfeda7559e3
Bijan <bijan2059@gmail.com>
2024-09-17 13:08:06 +0545

    added another function for initate payment

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -4,6 +4,7 @@ const pInstance = new Payment()
 const paymentRouter = express.Router()
 
 paymentRouter.post('/initialize-esewa', pInstance.initializePayment);
+paymentRouter.post('/initialize-esewa2', pInstance.initializePayment2);
 paymentRouter.get('/complete-payment', pInstance.completePayment);
 
 module.exports =  paymentRouter;
